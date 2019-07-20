65349588774e3263c40b6d103cba1b5fd70e163a
bassamkdev <bassamkazemi.dev@gmail.com>
2019-07-19 21:57:23 -0700

    add a shopping page, collection preview and collection item to the app

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 
-const HatsPage = () => (
-  <h1>HATS PAGE</h1>
-)
+
 
 function App() {
   return (
     <div>
       <Route exact path='/' component={HomePage}/>
-      <Route exact path='/shop/hats' component={HatsPage}/>
+      <Route exact path='/shop' component={ShopPage}/>
     </div>
   );
 }
