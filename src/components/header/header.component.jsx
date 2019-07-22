45eafc676bc2c2f7bbc4d8a1be18a7fb550662b8
bassamkdev <bassamkazemi.dev@gmail.com>
2019-07-21 18:29:32 -0700

    implementing Sign Out and adding some style to sign in component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import './header.styles.scss';
 import {ReactComponent as Logo} from '../../assets/crown.svg'
 import {Link} from 'react-router-dom'
+import { auth } from '../../firebase/firebase.utils';
 
-const Header = () => (
+const Header = ({currentUser}) => (
     <div className='header'>
         <Link to='/'>
             <Logo className='logo-container'/>
@@ -11,6 +12,12 @@ const Header = () => (
         <div className='options'>
             <Link to='/shop' className='option'>SHOP</Link>
             <Link to='/contact' className='option'>CONTACT</Link>
+            {
+                currentUser ? 
+                    <div className='option' onClick={()=> auth.signOut()}>SIGN OUT</div>
+                :
+                    <Link to='/signin' className='option'>SIGN IN</Link>
+            }
         </div>
     </div>
 )
